fb0166fc471c027955ff92debf46117574c18902
Arona74 <alexandre.poete@gmail.com>
2026-06-30 14:52:37 +0200

    1.20.1

--- src/main/java/io/github/potassiummc/resetchunks/ResetChunks.java
+++ src/main/java/io/github/potassiummc/resetchunks/ResetChunks.java
@@ -1,14 +1,14 @@
 package io.github.potassiummc.resetchunks;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.ResetChunksCommand;
 
 public class ResetChunks implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
 			ResetChunksCommand.register(dispatcher);
 		});
 	}
